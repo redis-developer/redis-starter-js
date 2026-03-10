79a823c258b8cfa2b1418ca408b0ba6a08bedb2b
Will Johnston <will-github@wwj.dev>
2026-03-10 07:41:51 -0700

    removing unnecessary code

--- server/redis.ts
+++ server/redis.ts
@@ -1,6 +1,5 @@
 import { createClient } from "redis";
 import { config } from "./config.js";
-import { log } from "./log.js";
 
 export const redis = await createClient({ url: config.REDIS_URL })
   .on("error", (err) => {
